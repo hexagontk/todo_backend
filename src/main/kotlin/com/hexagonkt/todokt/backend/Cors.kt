3bcbc472d750893f950ea49642bc01c5ad331804
Johanne <j.andersen864@gmail.com>
2019-10-04 19:28:08 +0200

    Try to fix headers [#166]

--- src/main/kotlin/com/hexagonkt/todokt/backend/Cors.kt
+++ src/main/kotlin/com/hexagonkt/todokt/backend/Cors.kt
@@ -14,7 +14,7 @@ fun Router.cors(
         response.setHeader("Access-Control-Allow-Origin", origin)
         response.setHeader("Access-Control-Allow-Credentials", credentials)
         val requestHeaders = if (headers.isEmpty()) request.headers.keys.toSet() else headers
-        response.setHeader("Access-Control-Allow-Headers", requestHeaders.joinToString(","))
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept")
         response.setHeader("Access-Control-Request-Method", methods.joinToString(","))
     }
 }
