519523ea54788eac1bc6a619c873113f9bee5d13
Johanne <j.andersen864@gmail.com>
2019-10-05 07:36:12 +0200

    Clean up cors and add default value to completed in a task [#166]

--- src/main/kotlin/com/hexagonkt/todokt/backend/Cors.kt
+++ src/main/kotlin/com/hexagonkt/todokt/backend/Cors.kt
@@ -4,17 +4,11 @@ import com.hexagonkt.http.ALL
 import com.hexagonkt.http.Method
 import com.hexagonkt.http.server.Router
 
-fun Router.cors(
-    origin: String = "*",
-    methods: Set<Method> = ALL,
-    headers: Set<String> = emptySet(),
-    credentials: Boolean = true) {
+fun Router.cors() {
 
     before {
-        response.setHeader("Access-Control-Allow-Origin", origin)
-        response.setHeader("Access-Control-Allow-Credentials", credentials)
-        val requestHeaders = if (headers.isEmpty()) request.headers.keys.toSet() else headers
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept")
-        response.setHeader("Access-Control-Request-Method", methods.joinToString(","))
+        response.setHeader("Access-Control-Allow-Origin", "*")
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type")
+        response.setHeader("Access-Control-Request-Method", "GET,POST,PATCH,DELETE")
     }
 }
--- src/main/kotlin/com/hexagonkt/todokt/backend/entities/Task.kt
+++ src/main/kotlin/com/hexagonkt/todokt/backend/entities/Task.kt
@@ -6,7 +6,7 @@ data class Task(
     val id: String,
     val title: String,
     val order: Int? = null,
-    val completed: Boolean? = null
+    val completed: Boolean? = false
 ){
     val url = "${SettingsManager.requireSetting("serviceUrl") as String}/tasks/$id"
 }
