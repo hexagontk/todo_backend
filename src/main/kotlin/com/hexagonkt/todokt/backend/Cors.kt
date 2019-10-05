5ff386e92d350747099a1d6a0146a763a646f59c
Johanne <j.andersen864@gmail.com>
2019-10-05 07:42:28 +0200

    Add OPTIONS back as an allowed header... [#166]

--- src/main/kotlin/com/hexagonkt/todokt/backend/Cors.kt
+++ src/main/kotlin/com/hexagonkt/todokt/backend/Cors.kt
@@ -7,6 +7,6 @@ fun Router.cors() {
     before {
         response.setHeader("Access-Control-Allow-Origin", "*")
         response.setHeader("Access-Control-Allow-Headers", "Content-Type")
-        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE")
+        response.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,DELETE,OPTIONS")
     }
 }
